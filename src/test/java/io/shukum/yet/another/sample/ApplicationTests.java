64113fa3caf7e79e7a6c286bea74e89b1c97646b
Shubham Kumar <shubham908@gmail.com>
2025-02-15 23:31:22 +0530

    Initial commit

---
+++ src/test/java/io/shukum/yet/another/sample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shukum.yet.another.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
